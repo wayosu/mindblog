3193d2dfaf8bd49fa645a19a030aa8bc138e76ef
Wahyu Setiawan Usman <wahyusman10@gmail.com>
2023-12-10 00:05:20 +0800

    add files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
+import Featured from "@/components/Featured/Featured";
 import styles from "./homepage.module.css";
+import CategoryList from "@/components/CategoryList/CategoryList";
+import CardList from "@/components/CardList/CardList";
+import Menu from "@/components/Menu/Menu";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className="styles.content">
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Featured/Featured.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
+
+const Featured = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, minders!</b> {"Let's"} make a post out of your thoughts.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            vel qui eum quibusdam eius iste error ratione, voluptatem, non
+            eveniet omnis quia dolorum, ut illo praesentium est. Corrupti ipsum
+            mollitia dolorem minus, esse nulla blanditiis repellat quibusdam
+            dolore officia. Vitae.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
